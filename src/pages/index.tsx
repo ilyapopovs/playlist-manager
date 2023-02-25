42dddfbefb1d86fccbe23e674c7e228d7e9ac825
ilyapopovs <ilya@popovs.dev>
2023-02-25 06:39:17 +0200

    move Layout to _app, as per Next docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import { type NextPage } from "next";
-import { Layout } from "@/components/Layout";
 import { api } from "@/utils/api";
 import Link from "next/link";
 
@@ -7,21 +6,19 @@ const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
-    <Layout>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-slate-900">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Placeholder title
-          </h1>
-          <Link href="/page2" className="font-bold text-white">
-            Go to page 2
-          </Link>
-          <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
-      </main>
-    </Layout>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-slate-900">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          Placeholder title
+        </h1>
+        <Link href="/page2" className="font-bold text-white">
+          Go to page 2
+        </Link>
+        <p className="text-2xl text-white">
+          {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+        </p>
+      </div>
+    </main>
   );
 };
 
--- src/pages/page2/index.tsx
+++ src/pages/page2/index.tsx
@@ -1,21 +1,18 @@
 import { type NextPage } from "next";
-import { Layout } from "@/components/Layout";
 import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <Layout>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-slate-900">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Page 2
-          </h1>
-          <Link href="/" className="font-bold text-white">
-            Go to root
-          </Link>
-        </div>
-      </main>
-    </Layout>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-slate-900">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          Page 2
+        </h1>
+        <Link href="/" className="font-bold text-white">
+          Go to root
+        </Link>
+      </div>
+    </main>
   );
 };
 
