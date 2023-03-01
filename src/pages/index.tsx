ba8935ea6ca756eec7ca441c744301f5c11dcb32
ilyapopovs <ilya@popovs.dev>
2023-03-01 23:30:58 +0200

    minor style adjustments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
 import { type NextPage } from 'next'
 import Head from 'next/head'
-
-import { api } from '@/utils/api'
 import Link from 'next/link'
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: 'from tRPC' })
-
   return (
     <>
       <Head>
@@ -14,17 +10,12 @@ const Home: NextPage = () => {
         <meta content="Manage your Spotify playlists!" name="description" />
         <link href="/favicon.ico" rel="icon" />
       </Head>
-      <main className="flex flex-col items-center justify-center bg-slate-900">
+      <main className="flex flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Placeholder title
           </h1>
-          <Link className="text-white" href={'/app'}>
-            Go to App
-          </Link>
-          <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : 'Loading tRPC query...'}
-          </p>
+          <Link href={'/app'}>Go to App</Link>
         </div>
       </main>
     </>
